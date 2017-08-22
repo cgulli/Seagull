3a45b5e520449630ee9b925787ca3c4062719e63
cgulli <caitlin.gulliford@gmail.com>
2017-08-22 14:56:36 +0100

    we removed send email as not working on ubuntu but will add it again later

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   var fact = getFact();
-  var email = getEmail(fact);
-  email.send(function(err){
-    if (err) {
-      res.end(err.concat("\n"));
-    }
-    else {
-      res.end("Email sent!!".concat("\n"));
-    }
-  });
+  console.log("Email would be sent now.");
 }).listen(8080, 'localhost');
 
 console.log('Server running at http://localhost:8080/');
